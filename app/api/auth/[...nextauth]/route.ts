7ed59315f5a1d21cba4144aeda6cdd6f493e57c1
marvemakuyana <marvellousmakuyana@gmail.com>
2024-03-04 17:56:13 +0200

    prisma

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,13 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import prisma from "@/lib/prismadb";
+import { Adapter } from "next-auth/adapters";
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
